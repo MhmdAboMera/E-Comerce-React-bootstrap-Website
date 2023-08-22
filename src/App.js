c43cc4549b1bffce709b63e7181e045c54a7f29a
Your Name <you@example.com>
2023-08-22 23:03:00 +0300

    Upload the Desing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./componetes/AppNavbar";
+import HomePage from "./componetes/HomePage";
+import Contact from "./componetes/Contact";
+import About from "./componetes/About";
+import Login from "./componetes/Login";
+import Sidbar from "./componetes/Sidbar";
+import Products from "./componetes/Products";
+import Productdetails from "./componetes/Productdetails";
+import Cards from "./componetes/Cards";
+import Categories from "./componetes/Categories";
+import Categoriesdetails from "./componetes/Categoriesdetails";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Navbar />
+      <div className="row contant">
+        <div
+          className="sidbar col-2 text-white-50 d-none d-lg-block"
+          id="sidbar"
         >
-          Learn React
-        </a>
-      </header>
+          <Sidbar />
+        </div>
+        <div className=" col-8 flex-1">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:productId" element={<Productdetails />} />
+            <Route path="/products/categories" element={<Categories />} />
+            <Route path="/cards" element={<Cards />} />
+            <Route
+              path="/products/category/:categoryName"
+              element={<Categoriesdetails />}
+            />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
